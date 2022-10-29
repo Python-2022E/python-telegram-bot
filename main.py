371aa66c387aa3f9cb95db126e724c42c9e151db
Jalilov <javohirjalilov777@gmail.com>
2022-10-29 15:21:07 +0500

    send Photo

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ bot = telegram.Bot(TOKEN)
 update = bot.getUpdates()[-1]
 chat_id = update.message.chat.id
 text = update.message.text
+img = 'https://c.ndtvimg.com/2020-08/h5mk7js_cat-generic_625x300_28_August_20.jpg'
 
-bot.sendMessage(chat_id, text)
+bot.sendPhoto(chat_id, photo=img)
