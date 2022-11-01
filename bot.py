da96c2fbf0d0a44bf664605f806207f069f419f4
Jalilov <javohirjalilov777@gmail.com>
2022-11-01 15:06:23 +0500

    message handler

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
 from email import message
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 def start(update, context):
     chat_id = update.message.chat.id
-    # text = update.message.text
-
     bot = context.bot
-    
+
     bot.sendMessage(chat_id, 'Welcome!')
 
+def echo(update, context):
+    chat_id = update.message.chat.id
+    text = update.message.text
+    bot = context.bot
+
+    bot.sendMessage(chat_id, text)
 
 updater = Updater("5559122728:AAERqDDQSGzmbuY0jZklQBawNd3Bt0m5xqc")
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
 updater.start_polling()
 updater.idle()
