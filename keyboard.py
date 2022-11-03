63a4c2a0994eadd397fce96f7d322fccc149a9bd
Jalilov <javohirjalilov777@gmail.com>
2022-11-03 15:45:24 +0500

    cat and dog button

--- keyboard.py
+++ keyboard.py
@@ -1,14 +1,31 @@
-import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import json
 from telegram import KeyboardButton, ReplyKeyboardMarkup
+def start(update, context):
+    chat_id = update.message.chat.id
+    bot = context.bot
 
-TOKEN='5559122728:AAERqDDQSGzmbuY0jZklQBawNd3Bt0m5xqc'
-bot = telegram.Bot(TOKEN)
+    bot.sendMessage(chat_id, 'Welcome!')
 
-button = KeyboardButton(text='BUTTON')
-keyboard = ReplyKeyboardMarkup([[button]])
+def echo(update, context):
+    chat_id = update.message.chat.id
+    text = update.message.text
+    bot = context.bot
+    button1 = KeyboardButton(text='Cat')
+    button2 = KeyboardButton(text='Dog')
 
-update = bot.getUpdates()[-1]
-chat_id = update.message.chat.id
-text = update.message.text
+    reply_markup =ReplyKeyboardMarkup(
+        [
+            [button1, button2]
+        ],
+        resize_keyboard=True)
 
-bot.sendMessage(chat_id, text, reply_markup=keyboard)
+    bot.sendMessage(chat_id, text, reply_markup=reply_markup)
+
+updater = Updater("5559122728:AAERqDDQSGzmbuY0jZklQBawNd3Bt0m5xqc")
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+
+updater.start_polling()
+updater.idle()
