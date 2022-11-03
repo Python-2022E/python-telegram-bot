7538567db0fd6a178b09caf9c212c2c6bb2a6202
Jalilov <javohirjalilov777@gmail.com>
2022-11-03 15:59:28 +0500

    inline keyboard

--- keyboard.py
+++ keyboard.py
@@ -1,6 +1,13 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import (
+    Updater,
+    CommandHandler, 
+    MessageHandler, 
+    Filters,
+    InlineQueryHandler,
+    CallbackQueryHandler
+)
 import json
-from telegram import KeyboardButton, ReplyKeyboardMarkup
+from telegram import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 def start(update, context):
     chat_id = update.message.chat.id
     bot = context.bot
@@ -13,19 +20,33 @@ def echo(update, context):
     bot = context.bot
     button1 = KeyboardButton(text='Cat')
     button2 = KeyboardButton(text='Dog')
+    button3 = KeyboardButton(text='Inline')
 
     reply_markup =ReplyKeyboardMarkup(
         [
-            [button1, button2]
+            [button1, button2],
+            [button3]
         ],
         resize_keyboard=True)
 
     bot.sendMessage(chat_id, text, reply_markup=reply_markup)
 
+def inlinekeyboard(update, context):
+    chat_id = update.message.chat.id
+    text = update.message.text
+    bot = context.bot
+    botton = InlineKeyboardButton(text='Your Channel', callback_data='channel_link')
+    keyboard = InlineKeyboardMarkup([[botton]])
+
+    bot.sendMessage(chat_id, text, reply_markup = keyboard)
+
+def callback_inline(update, context):
+    print('INLINE')
 updater = Updater("5559122728:AAERqDDQSGzmbuY0jZklQBawNd3Bt0m5xqc")
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text('Inline'), inlinekeyboard))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
-
+updater.dispatcher.add_handler(CallbackQueryHandler(callback_inline))
 updater.start_polling()
 updater.idle()
